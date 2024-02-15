f74193ccfdcf2c13675c6cb2316fbf2528d81d76
rskorlygin <rskorlygin>
2024-02-15 22:42:39 +0300

    Add HelloWorldTest

---
+++ src/test/java/HelloMyNameTest.java
@@ -0,0 +1,11 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class HelloMyNameTest {
+
+    @Test
+    public void testHelloMyName() {
+        System.out.println("Hello from Roman");
+    }
+}
