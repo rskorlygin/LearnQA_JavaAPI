527a57c9e9f0b199d39c0d834903381feaf1a29a
rskorlygin <rskorlygin>
2024-02-29 20:16:40 +0300

    Add Ex6: Redirects

--- src/test/java/HelloMyNameTest.java
+++ src/test/java/HelloMyNameTest.java
@@ -1,7 +1,11 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloMyNameTest {
 
@@ -19,7 +23,7 @@ public class HelloMyNameTest {
     }
 
     @Test
-    public void testJsonParsing() {
+    public void testEx5JsonParsing() {
         JsonPath response = RestAssured
                 .get("https://playground.learnqa.ru/api/get_json_homework")
                 .jsonPath();
@@ -27,5 +31,18 @@ public class HelloMyNameTest {
 
         String answer = response.get("messages[1].message");
         System.out.println(answer);
+    }
+
+    @Test
+    public void testEx6Redirects() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(true)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+        String locationHeaders = response.getHeader("X-Host");
+        System.out.println(locationHeaders);
     }
 }
