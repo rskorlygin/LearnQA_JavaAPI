eed173d7dbe94e28dd8c48de304cb456824ff639
rskorlygin <rskorlygin>
2024-02-29 19:58:16 +0300

    Add Ex5: Parsing JSON

--- src/test/java/HelloMyNameTest.java
+++ src/test/java/HelloMyNameTest.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
@@ -15,5 +16,16 @@ public class HelloMyNameTest {
                 .get("https://playground.learnqa.ru/api/get_text")
                 .andReturn();
         response.prettyPrint();
+    }
+
+    @Test
+    public void testJsonParsing() {
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+        response.prettyPrint();
+
+        String answer = response.get("messages[1].message");
+        System.out.println(answer);
     }
 }
